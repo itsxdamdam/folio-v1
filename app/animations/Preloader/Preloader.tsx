9d8c407abba16d6d91ca6786a4ed97770983dc1a
Bakare Samuel Oluwadamilola <49197462+itsxdamdam@users.noreply.github.com>
2025-05-10 21:52:38 +0100

    feat: content of preloader

---
+++ app/animations/Preloader/Preloader.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Screen = () => {
+  return (
+    <div className="h-screen bg-[#0E1016] text-[#E4DED7] flex flex-col justify-center items-center text-left">
+      <p className="w-xl p-3">
+        The sole purpose of technology is to solve problems, build usable and{" "}
+        <br />
+        reusable solutions, leaving a positive impact on anyone that interacts
+        with it. 
+      </p>
+      <p className="text-right justify-self-end w-xl font-bold py-3">
+        ━━━ Bakare Samuel <br />
+        ‘87
+      </p>
+    </div>
+  );
+};
+
+export default Screen;
---
+++ app/context/DarkModeContext.tsx
@@ -0,0 +1,34 @@
+import { createContext, ReactNode, useState } from "react";
+
+type darkModeType = {
+  darkMode: boolean;
+  toggleDarkMode: () => void;
+};
+
+const DarkModeContext = createContext<darkModeType>({
+  darkMode: true,
+  toggleDarkMode: () => {},
+});
+
+type Props = {
+  children: ReactNode;
+};
+
+function DarkModeProvider({ children }: Props) {
+  const [darkMode, setDarkMode] = useState(true);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prev) => !prev);
+    
+  };
+
+  return (
+    <div>
+      <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
+        {children}
+      </DarkModeContext.Provider>
+    </div>
+  );
+}
+
+export { DarkModeContext, DarkModeProvider };
