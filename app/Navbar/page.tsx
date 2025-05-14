acec09b9b5ef38e5b0c760591e6376831cec705a
Bakare Samuel Oluwadamilola <49197462+itsxdamdam@users.noreply.github.com>
2025-05-14 20:37:31 +0100

    fix: comment out nav

--- app/Navbar/page.tsx
+++ app/Navbar/page.tsx
@@ -1,24 +1,24 @@
-import React, { useContext } from "react";
-import { DarkModeContext } from "../context/DarkModeContext";
-// import Screen from "../animations/Preloader/Screen";
-const Navbar = () => {
-  const { darkMode, toggleDarkMode } = useContext(DarkModeContext);
-  console.log("navbar-drk", darkMode);
+// import React, { useContext } from "react";
+// import { DarkModeContext } from "../context/DarkModeContext";
+// // import Screen from "../animations/Preloader/Screen";
+// const Navbar = () => {
+//   const { darkMode, toggleDarkMode } = useContext(DarkModeContext);
+//   console.log("navbar-drk", darkMode);
 
-  // const handleClick = () => {
-  //   toggleDarkMode()
-  // }
+//   // const handleClick = () => {
+//   //   toggleDarkMode()
+//   // }
 
-  return (
-    <div className={darkMode ? "bg-black" : "bg-white"}>
-      {`${darkMode} is here`}
-      <br />
-      Navbar <br />
-      <button onClick={toggleDarkMode}>Change Mode Button</button>
-      <br />
-      current theme: {darkMode ? "dark" : "light"}
-    </div>
-  );
-};
+//   return (
+//     <div className={darkMode ? "bg-black" : "bg-white"}>
+//       {`${darkMode} is here`}
+//       <br />
+//       Navbar <br />
+//       <button onClick={toggleDarkMode}>Change Mode Button</button>
+//       <br />
+//       current theme: {darkMode ? "dark" : "light"}
+//     </div>
+//   );
+// };
 
-export default Navbar;
+// export default Navbar;
