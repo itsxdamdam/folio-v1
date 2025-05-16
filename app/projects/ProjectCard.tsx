41a88552027cddd8d09ee25f11f7259373c14700
Bakare Samuel Oluwadamilola <49197462+itsxdamdam@users.noreply.github.com>
2025-05-16 18:33:13 +0100

    fix: project links on new tabs

--- app/projects/ProjectCard.tsx
+++ app/projects/ProjectCard.tsx
@@ -16,16 +16,26 @@ const ProjectCard = ({
     <div className="relative project-card p-7 justify-center text-white bg-[#20273A] rounded-3xl my-4 h-[720px] md:h-[800px] lg:h-[500px] w-full">
       <div className="h-full flex flex-col lg:justify-between md:justify-start md:w-full lg:w-[40%] xl:w-[60%] ">
         <div className="links">
-          <Link href={live_link} className="font-bold pr-3 md:text-2xl">
+          <Link
+            href={live_link}
+            target="_blank"
+            className="font-bold pr-3 md:text-2xl"
+          >
             Live Link
           </Link>
-          <Link href={github} className="font-bold px-3 md:text-2xl">
+          <Link
+            href={github}
+            target="_blank"
+            className="font-bold px-3 md:text-2xl"
+          >
             Github
           </Link>
         </div>
         <div className="">
           <p className="font-extrabold text-[24px]">{name}</p>
-          <p className="text-[18px] mt-2 leading-8 lg:flex-wrap">{description}</p>
+          <p className="text-[18px] mt-2 leading-8 lg:flex-wrap">
+            {description}
+          </p>
         </div>
         <div className="flex gap-4 items-start my-3 font-bold md:text-xl flex-wrap md:flex-wrap lg:flex-wrap">
           {technologies.map((tech, id) => (
